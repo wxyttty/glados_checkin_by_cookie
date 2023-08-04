dfc2ccd4fb1dddb80bc03a654cc5d9d41d4dcba1
Shawn Rita <113410236+gaeduf@users.noreply.github.com>
2023-08-04 09:49:56 +0800

    Update glados_checkin_cookie.py

--- glados_checkin_cookie.py
+++ glados_checkin_cookie.py
@@ -6,7 +6,7 @@
 
 import requests
 import json
-
+COOKIE = os.environ.get('COOKIE')
 url = "https://glados.rocks/api/user/checkin"
 payload = {
     'token': 'glados.one'
@@ -14,7 +14,7 @@ payload = {
 headers = {
   "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
   "Content-Type": "application/json;charset=UTF-8",
-  "Cookie": "koa:sess=eyJ1c2VySWQiOjE3MDkzMCwiX2V4cGlyZSI6MTcxNjg2MjQxNzg1MiwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=SjVVHihCG-Qu0B6TFv1KllV6G08;"
+  "Cookie": COOKIE
 }
 
 data = json.dumps(payload)
